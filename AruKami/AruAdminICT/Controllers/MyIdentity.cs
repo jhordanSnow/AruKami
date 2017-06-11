0df77d4a7150bf7aa73648f6d3ae3b74ce2abe3e
cgranados16 <cgranados16@gmail.com>
2017-06-11 16:35:27 -0600

    Login Jorditan  y ICT

---
+++ AruKami/AruAdminICT/Controllers/MyIdentity.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using AruAdmin.Models;
+
+namespace AruAdmin.Controllers
+{
+    public class MyIdentity : IIdentity
+    {
+        public IIdentity Identity { get; set; }
+        public View_User User { get; set; }
+
+        public MyIdentity(View_User user)
+        {
+            Identity = new GenericIdentity(user.Username);
+            User = user;
+        }
+
+        public string AuthenticationType
+        {
+            get { return Identity.AuthenticationType; }
+        }
+
+        public bool IsAuthenticated
+        {
+            get { return Identity.IsAuthenticated; }
+        }
+
+        public string Name
+        {
+            get { return Identity.Name; }
+        }
+
+
+        public class MyPrincipal : IPrincipal
+        {
+            private readonly MyIdentity MyIdentity;
+            public MyPrincipal(MyIdentity _myIdentity)
+            {
+                MyIdentity = _myIdentity;
+            }
+            public IIdentity Identity
+            {
+                get { return MyIdentity; }
+            }
+
+            public bool IsInRole(string role)
+            {
+                return Roles.IsUserInRole(role);
+            }
+        }
+    }
+
+
+    public class MyMembershipProvider : MembershipProvider
+    {
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EnablePasswordReset
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool EnablePasswordRetrieval
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int MaxInvalidPasswordAttempts
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredPasswordLength
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int PasswordAttemptWindow
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override MembershipPasswordFormat PasswordFormat
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string PasswordStrengthRegularExpression
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresQuestionAndAnswer
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresUniqueEmail
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // Here In this example we will use only ValidateUser method, we will see remaining later like create user, 
+        //update user change password and more 
+
+        public override bool ValidateUser(string username, string password)
+        {
+            using (AruKamiEntities dc = new AruKamiEntities())
+            {
+                ObjectParameter Output = new ObjectParameter("responseMessage", typeof(string));
+                dc.PR_AdminLogin(username, password, Output);
+
+
+                if (Output.Value.ToString().Equals("User successfully logged in"))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ AruKami/AruAdminICT/Filter/ClaimsExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace AruAdmin.Filter
+{
+    public static class ClaimsExtensions
+    {
+        static string GetUserEmail(this ClaimsIdentity identity)
+        {
+            return identity.Claims?.FirstOrDefault(c => c.Type == "Tabemashou_Admin.Models.RegisterViewModel.Email")?.Value;
+        }
+
+        public static string GetUserEmail(this IIdentity identity)
+        {
+            var claimsIdentity = identity as ClaimsIdentity;
+            return claimsIdentity != null ? GetUserEmail(claimsIdentity) : "";
+        }
+
+        static string GetUserNameIdentifier(this ClaimsIdentity identity)
+        {
+            return identity.Claims?.FirstOrDefault(c => c.Type == "Tabemashou_Admin.Models.RegisterViewModel.NameIdentifier")?.Value;
+        }
+
+        public static string GetUserNameIdentifier(this IIdentity identity)
+        {
+            var claimsIdentity = identity as ClaimsIdentity;
+            return claimsIdentity != null ? GetUserNameIdentifier(claimsIdentity) : "";
+        }
+    }
+}
